752a965c9eca8f7d4bbd1318c05b6cebe0fc98c4
szymonjanas <szymon.janas.95@gmail.com>
2018-07-16 12:39:20 +0200

    Update database.hpp

--- database.hpp
+++ database.hpp
@@ -12,6 +12,6 @@ public:
 
     void addStudentBack(std::string name, std::string surname); //here adding on back with next deflaut index number
     void addStudentIndex(unsigned short int index, std::string name, std::string surname); //here adding on back with index and sort to take it in right place
-    void deleteStudent(unsigned short int); // delete student with index
+    void deleteStudent(unsigned short int); // delete student by index
     void showList(); // show students list
 };
